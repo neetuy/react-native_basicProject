89338276de25bbefeb43b0f3cef62b191bf3b8ad
neetu <neetuy1455@gmail.com>
2017-05-29 12:10:15 +0530

    first commit

---
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -0,0 +1,39 @@
+package com.awesomeproject;
+
+import com.facebook.react.ReactActivity;
+
+// public class MainActivity extends ReactActivity {
+
+//     /**
+//      * Returns the name of the main component registered from JavaScript.
+//      * This is used to schedule rendering of the component.
+//      */
+//     @Override
+//     protected String getMainComponentName() {
+//         return "AwesomeProject";
+//     }
+// }
+
+import org.pgsqlite.SQLitePluginPackage;
+
+public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
+
+    private ReactInstanceManager mReactInstanceManager;
+    private ReactRootView mReactRootView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mReactRootView = new ReactRootView(this);
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")  // this is dependant on how you name you JS files, example assumes index.android.js
+                .setJSMainModuleName("index.android")        // this is dependant on how you name you JS files, example assumes index.android.js
+                .addPackage(new MainReactPackage())
+                .addPackage(new SQLitePluginPackage())       // register SQLite Plugin here
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+        mReactRootView.startReactApplication(mReactInstanceManager, "AwesomeProject", null); //change "AwesomeProject" to name of your app
+        setContentView(mReactRootView);
+    }
